7e2a132618fe5b0556d60d0f1b7f3388ae7e9577
Guo Zirui <2125561933@qq.com>
2018-12-22 16:51:19 +0900

    Add files via upload

---
+++ AssWeTran.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+import chardet
+import codecs
+import re
+import ass
+
+
+SourceFileName = input('请输入文件地址：')
+
+# 检查文件编码
+SourceFile = open(SourceFileName, 'rb')
+FileEnco = chardet.detect(SourceFile.read()).get('encoding')
+SourceFile.close()
+# 按文件编码重新打开文件
+SourceFile = codecs.open(SourceFileName, 'rb', FileEnco)
+TargetFile = codecs.open(SourceFileName + '.srt', 'w', 'utf-8')
+
+AssFile = ass.parse(SourceFile)
+TimeDic = dict()
+
+for event in AssFile.events:
+    if not re.match('//', event.text) and not event.text == '':
+        StartTime = str(event.start)
+        EndTime = str(event.end)
+        StartTime = '0' + StartTime
+        StartTime = StartTime[:-3]
+        StartTime = re.sub(r'\.', ',', StartTime)
+        EndTime = '0' + EndTime
+        EndTime = EndTime[:-3]
+        EndTime = re.sub(r'\.', ',', EndTime)
+        Time = StartTime + ' --> ' + EndTime
+        SubTitle = re.sub(r'{[^}]*}', '', event.text)
+        SubTitle = re.sub(r'\\N', '\n', SubTitle)
+        if not Time in TimeDic.keys():
+            TimeDic[Time] = SubTitle
+        else:
+            TimeDic[Time] = TimeDic[Time] + '\n' + SubTitle
+
+i = 1
+
+for key, value in TimeDic.items():
+    # if value != '':
+    TargetFile.write(str(i))
+    TargetFile.write('\n')
+    TargetFile.write(key)
+    TargetFile.write('\n')
+    TargetFile.write(value)
+    TargetFile.write('\n')
+    TargetFile.write('\n')
+    i = i + 1
